00ae4293681e4ef764fbb71c5f03302f1b85069d
hidejiochi <platitude000@gmail.com>
2016-11-04 22:38:26 +0900

    更新

--- Assets/MMM/Scripts/ButtonGenerator.cs
+++ Assets/MMM/Scripts/ButtonGenerator.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class ButtonGenerator : MonoBehaviour {
+public class ButtonGenerator : MonoBehaviour
+{
 
-    public GameObject StageSelectButtonPrefab;
-
-    
+    public StageSelectButton StageSelectButtonPrefab;
+    [SerializeField]
+    private Transform _contentTransform;
+    [SerializeField]
+    public List<string> _sceneStringList;
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	
+    void Start()
+    {
+
+        for (int i = 0; i < _sceneStringList.Count; i++)
+        {
+            //この中でボタンを生成する
+            StageSelectButton stageSelectButton = Instantiate(StageSelectButtonPrefab);
+            //Scroll ViewのContentを親にする
+            stageSelectButton.transform.SetParent(_contentTransform, false);
+            //初期化
+            string stageStr = _sceneStringList[i];
+            string stageNum = (i + 1).ToString();
+            stageSelectButton.Initialize(stageStr, stageNum);
+        }
+    }
 }
--- Assets/MMM/Scripts/StageSelectButton.cs
+++ Assets/MMM/Scripts/StageSelectButton.cs
@@ -9,12 +9,16 @@ public class StageSelectButton : MonoBehaviour
     private Button _button;
     [SerializeField]
     private string _sceneName;
+    [SerializeField]
+    private Text _text;
 
-    /// <summary>
-    /// Initialize this instance.
-    /// </summary>
-    private void Start()
+    public void Initialize(string sceneName, string buttonText)
     {
+        //飛ばすシーンを設定
+        _sceneName = sceneName;
+        //テキストの文字を変更
+        _text.text = buttonText;
+        //ボタンを押した時
         _button.onClick.AddListener(() => {
             SceneManager.LoadScene(_sceneName);
         });
