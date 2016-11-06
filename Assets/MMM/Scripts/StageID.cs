2b8e9e03a5dc4d78e35bea5f41afd0d727928064
hidejiochi <platitude000@gmail.com>
2016-11-06 15:54:54 +0900

    更新

---
+++ Assets/MMM/Scripts/StageID.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageID : MonoBehaviour {
+
+    [SerializeField]
+    private List<string> saveList;  
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
