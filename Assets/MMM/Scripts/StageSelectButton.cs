4a6603279dfb08f80d2ba46bf0e92f49de36d8eb
hidejiochi <platitude000@gmail.com>
2016-11-08 18:33:17 +0900

    更新

--- Assets/MMM/Scripts/StageSelectButton.cs
+++ Assets/MMM/Scripts/StageSelectButton.cs
@@ -2,15 +2,24 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class StageSelectButton : MonoBehaviour
-{
+{   
     [SerializeField]
     private Button _button;
     [SerializeField]
     private string _sceneName;
     [SerializeField]
     private Text _text;
+
+    [SerializeField]
+    private List<string> saveList;
+
+    public List<string> SaveList
+    {
+        get { return saveList; }
+    }
 
     public void Initialize(string sceneName, string buttonText)
     {
@@ -22,5 +31,11 @@ public class StageSelectButton : MonoBehaviour
         _button.onClick.AddListener(() => {
             SceneManager.LoadScene(_sceneName);
         });
+        //_sceneNameがSaveListに含まれているか
+        if (SaveList.Contains(_sceneName) == true)
+        {
+            //色を変更する
+            _button.image.color = Color.red;
+        }
     }
 }
