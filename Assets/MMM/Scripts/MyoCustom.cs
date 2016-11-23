35d522dd9109fd8538413b1e0bb2591002a10738
hidejiochi <platitude000@gmail.com>
2016-11-23 19:38:18 +0900

    no message

---
+++ Assets/MMM/Scripts/MyoCustom.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyoCustom : MonoBehaviour {
+
+    [SerializeField]
+    private List<Mesh> _meshList;
+
+    public Mesh GetMesh(string id)
+    {
+        foreach (Mesh mesh in _meshList)
+        {
+            if (mesh.name == id)
+            {
+                return mesh;
+            }
+        }
+        Debug.LogError("指定したidのMeshが存在しません");
+        return null;
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
