8fca537281121ddb29a7817fa35dd922a4d9a8ba
hidejiochi <platitude000@gmail.com>
2016-11-27 17:15:44 +0900

    no message

--- Assets/MMM/Scripts/BodyPartsButton.cs
+++ Assets/MMM/Scripts/BodyPartsButton.cs
@@ -10,14 +10,15 @@ public class BodyPartsButton : MonoBehaviour {
 
     [SerializeField]
     private Text _text;
-
+     
     public void Initialize(string bodyPartsName, string buttonText)
-    {        
+    {
+        
         //テキストの文字を変更
         _text.text = buttonText;
         //ボタンを押した時
         _button.onClick.AddListener(() => {
-            MyoCustom.ChangeParts("Body"+対応したID);
+            GameObject.Find("Myo").GetComponent<MyoCustom>().ChangeParts("Id");
         });
          
     }
